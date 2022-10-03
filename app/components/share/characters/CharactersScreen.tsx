8f4ce0594f50179fc79d206d5416174d03164947
Denis <retusin83@gmail.com>
2022-10-03 14:33:23 +0300

    Feat: Add MobileCharacterPage

--- app/components/share/characters/CharactersScreen.tsx
+++ app/components/share/characters/CharactersScreen.tsx
@@ -18,13 +18,13 @@ export const CharactersScreen: FC = () => {
   if (!characters) return null;
 
   return (
-    <div className='py-10'>
-      <div className='mainContainer bg-white rounded-xl shadow-2xl '>
+    <div className='py-10 container px-8 mx-auto'>
+      <div className='bg-white rounded-xl shadow-2xl '>
         <Hero
           title='Anime characters'
           description='Below is the entire list of Anime characters. Here you will find complete information about your favorite characters - height, weight, field of activity, pictures, photos, anime in which they participated and much more.'
         />
-        <ul className='px-6 grid grid-cols-6 gap-10 items-center py-8 text-center items-baseline'>
+        <ul className='px-6 grid  grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-10 items-center py-8 text-center items-baseline'>
           {characters.pages.map((page) =>
             page.data.map((repo: CharactersParams) => (
               <CharacterItem item={repo} key={repo.mal_id} />
