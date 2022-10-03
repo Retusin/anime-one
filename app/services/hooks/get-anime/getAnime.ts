3436304d30414a969bbc79b439316e19cb8087b5
Denis <retusin83@gmail.com>
2022-10-03 11:19:42 +0300

    feat: rq

--- app/services/hooks/get-anime/getAnime.ts
+++ app/services/hooks/get-anime/getAnime.ts
@@ -4,7 +4,7 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 export const getRequestAnime = () =>
   useInfiniteQuery(
     ['anime'],
-    ({ pageParam = 1 }) => AnimeServices.getAnimes({ limit: 7, pageParam }),
+    ({ pageParam = 1 }) => AnimeServices.getAnimes({ pageParam }),
     {
       getNextPageParam: (lastPage, allPages) => allPages.length + 1
     }
